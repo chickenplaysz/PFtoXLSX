b06504eba09df02dae2a90f7fbd35aeb75a6c27b
BuildTools <unconfigured@null.spigotmc.org>
2024-02-06 18:46:00 -0300

    Done a lot today: - Full Avenue PDF Reading - Checking and conversion of non text-based pdfs - Integrated data trasnfer from buffer to *app.xlsm

--- main.py
+++ main.py
@@ -8,7 +8,16 @@ import time
 import warnings
 import numpy as np
 import functools
+import os
 import swifter
+import random as rnd
+from forex_python.converter import CurrencyRates
+
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pyautogui
 
 
 warnings.simplefilter("ignore")
@@ -27,7 +36,12 @@ BUFFER_SHEET_PATH = "runtime/buffer_out.xlsx"
 class DocRender:
     def __init__(self, path):
         self.path = path
-        self.doc = fitz.Document(path)
+        if type(path) == str:
+            self.doc = fitz.Document(path)
+        else:
+            self.doc = path
+            self.path = path.name
+
         self.password = ""
 
         if self.doc.needs_pass:
@@ -37,13 +51,45 @@ class DocRender:
         else:
             self.password = None
 
-        if self.doc[0].get_text():
-            self.isocr = False
-        else:
-            self.isocr = True
-
         self.data = None
         self.corretora = None
+
+
+def ocr_check():
+    opt = webdriver.ChromeOptions()
+    opt.add_experimental_option("prefs", {
+        "download.default_directory": f"{os.getcwd()}\\runtime\\in"
+    })
+    driver = webdriver.Chrome(options=opt)
+    def selenium_ocr(path):
+        driver.get("https://www.ilovepdf.com/pt/ocr-pdf")
+        driver.find_element(By.ID, "pickfiles").click()
+        time.sleep(6)
+        pyautogui.write(os.path.abspath(path))
+        pyautogui.press("enter")
+
+        process_task = WebDriverWait(driver, 15).until(
+            EC.presence_of_element_located((By.ID, "processTask"))
+        )
+        process_task.click()
+        del process_task
+        time.sleep(8)
+
+        download = WebDriverWait(driver, 100).until(
+            EC.presence_of_element_located((By.ID, "pickfiles"))
+        )
+        download.click()
+        del download
+        time.sleep(2)
+
+
+    paths = list(glob.iglob("runtime\\in\\*.pdf"))
+    for path in paths:
+        doc = fitz.Document(path)
+        if not doc[0].get_text():
+            del doc
+            selenium_ocr(path)
+            os.remove(path)
 
 
 def get_stock_data(nome_ativo, x):
@@ -66,19 +112,17 @@ def write_to_buffer(documents):
     for document in documents:
         final = final.append(document, ignore_index=True)
 
-    with pd.ExcelWriter(BUFFER_SHEET_PATH) as writer:
+    with pd.ExcelWriter(BUFFER_SHEET_PATH, engine="openpyxl") as writer:
         final.to_excel(writer, sheet_name=BUFFER_SHEET_NAME, index=False, header=False)
 
 
 @functools.lru_cache(maxsize=100, typed=False)
-def read_db(ticker, dado) -> tuple:
-    match_row = B3_LOCAL_DATABASE[B3_LOCAL_DATABASE['ticker'].swifter.apply(lambda x: str(x) in str(ticker))]
+def read_db(ticker) -> tuple:
+    for row in B3_LOCAL_DATABASE.itertuples():
+        if row.ticker in ticker:
+            return row.cnpj
 
-    if not match_row.empty:
-        row = match_row.iloc[0]
-        return row[dado]
-
-    return "NÃO CADASTRADO NA B3"
+    return "NÃO CADASTRADO NA B3",
 
 
 def render_pdf(pdf_path: str):
@@ -90,15 +134,103 @@ def render_pdf(pdf_path: str):
             print("Senha incorreta\n")
             pass
 
-    if current_document.isocr:
+    text = current_document.doc[0].get_text()
+
+    if "inter dtvm" in text.lower():
         # TODO: Implement image recognition for inter if possible
         return None
 
-    text = current_document.doc[0].get_text()
-
     if "avenue" in text.lower():
-        # TODO: Create avenue rendering
-        return None
+        child_pages = []
+        data = pd.DataFrame()
+        current_document.corretora = "avenue"
+
+
+        current_document.doc.delete_page(0)
+        for index, page in enumerate(current_document.doc.pages()):
+            temp_document = fitz.Document()
+            temp_document.insert_pdf(current_document.doc, from_page=index, to_page=index, start_at=0)
+            temp_document[0].number = 0
+            child_pages.append(temp_document)
+
+        child_pages = [DocRender(page) for page in child_pages]
+
+        for index, page in enumerate(child_pages):
+            page.corretora = "avenue"
+            page.path = f"runtime\\in\\{rnd.randint(0, 100000)}_{index}.pdf"
+            page.doc.save(page.path)
+
+            if len(child_pages) == 1:
+                for i in range(6):
+
+                    current_table = tabula.read_pdf_with_template(
+                        page.path,
+                        template_path=f"runtime/templates/avenue_single_{i+1}.json",
+                        password=page.password,
+                        pages="all",
+                        pandas_options={"header": ["COMM", "Tran Fee", "Fees", "Number", "Net Amount", "Trade#"]}
+                    )[0]
+
+
+                    if current_table.swifter.progress_bar(False).apply(lambda a: True if a.isin(["SUMMARY FOR CURRENT TRADE DATE:"]).any() else False).any():
+                        break
+                    best = current_table
+                page.data = best
+                os.remove(page.path)
+
+            elif index == 0:
+                page.data = tabula.read_pdf_with_template(
+                    page.path,
+                    template_path="runtime/templates/avenue_first.json",
+                    password=page.password,
+                    pages="all",
+                    pandas_options={"header": ["Type"]}
+                )[0]
+                os.remove(page.path)
+
+
+            elif index == len(child_pages) - 1:
+                # TODO: avenue templates 1 to 8
+                for i in range(6):
+
+                    current_table = tabula.read_pdf_with_template(
+                        page.path,
+                        template_path=f"runtime/templates/avenue_last_{i+1}.json",
+                        password=page.password,
+                        pages="all",
+                        pandas_options={"header": ["COMM", "Tran Fee", "Fees", "Number", "Net Amount", "Trade#"]}
+                    )[0]
+                    if current_table.swifter.progress_bar(False).apply(lambda a: True if a.isin(["SUMMARY FOR CURRENT TRADE DATE:"]).any() else False).any():
+                        break
+                    best = current_table
+                page.data = best
+                os.remove(page.path)
+
+            else:
+                page.data = tabula.read_pdf_with_template(
+                    page.path,
+                    template_path="runtime/templates/avenue_normal.json",
+                    password=page.password,
+                    pages="all",
+                    pandas_options={"header": ["Type"]}
+                )[0]
+                os.remove(page.path)
+
+
+
+            page.data = page.data.dropna(axis=1, how="all")
+            page.data.columns = page.data.iloc[0]
+            page.data = page.data.drop(index=0)
+            data = data.append(page.data)
+
+        current_document.data = data.reset_index(drop=True)
+        del data
+        del current_table
+        del child_pages
+        del best
+        del temp_document
+
+        return current_document
 
     if "xp investimento" in text.lower():
         current_document.corretora = "xp"
@@ -109,7 +241,8 @@ def render_pdf(pdf_path: str):
                 pages="all",
                 pandas_options={"header": ["C/V"]}
         )
-    return current_document
+        return current_document
+    return None
 
 
 def parse_data(doc: DocRender):
@@ -174,14 +307,53 @@ def parse_data(doc: DocRender):
             "data_liquidacao": data_liquidacao,
             "data_pregao": data_pregao,
             "nome_corretora": nome_corretora,
-            "cnpj_corretora": cnpj_corretora
+            "cnpj_corretora": cnpj_corretora,
+            "corr": "xp"
         }
 
         return df
 
     if doc.corretora == "avenue":
-        # TODO: Implement avenue parsing
-        return None
+
+        doc.data = pd.merge(doc.data.iloc[::4, :].reset_index(), doc.data.iloc[1::4, :].reset_index(), left_index=True, right_index=True) \
+        .dropna(how="all", axis=1).drop(["index_x", "T_x", "P_x", "index_y", "Number_y", "Net Amount Trade#_y"], axis=1) \
+
+        doc.data[["type", "bs", "trade date"]] = doc.data["Type B/S Trade Date_x"].str.split(" ", expand=True)
+        doc.data[["net amount", "trade#"]] = doc.data["Net Amount Trade#_x"].str.split(" ", expand=True)
+        doc.data[["dsc", "name"]] = doc.data["Type B/S Trade Date_y"].str.split(":", expand=True)
+        doc.data = doc.data.drop([
+            "type",
+            "Type B/S Trade Date_x",
+            "Type B/S Trade Date_y",
+            "Net Amount Trade#_x",
+            "trade#",
+            "dsc",
+            "Fees_y",
+            "Number_x"
+        ], axis=1)
+        doc.data.columns = [
+            "data_liquidacao",
+            "quantidade",
+            "ticker",
+            "un",
+            "total",
+            "corretagem",
+            "taxa_transacao",
+            "taxa_outras",
+            "cv",
+            "data_pregao",
+            "preco_medio",
+            "nome"
+        ]
+        doc.data["cv"] = doc.data["cv"].map(lambda a: "C" if "B" in a else "V")
+        doc.data["corretagem"] = pd.to_numeric(doc.data["corretagem"])
+        doc.data["taxa_transacao"] = pd.to_numeric(doc.data["taxa_transacao"])
+        doc.data["taxa_outras"] = pd.to_numeric(doc.data["taxa_outras"])
+
+        return {
+            "transactions": doc.data,
+            "corr": "avenue"
+        }
 
     if doc.corretora == "inter":
         # TODO: Implement inter parsing
@@ -189,7 +361,6 @@ def parse_data(doc: DocRender):
 
 
 def format_data(doc: dict):
-    # TODO: all the necessary formating and optimization
     transactions = doc["transactions"].reset_index(drop=True)
     formated_data = pd.DataFrame(index=range(transactions.index.size), columns=[
         "data_pregao",
@@ -210,51 +381,102 @@ def format_data(doc: dict):
         "cnpj_empresa",
     ])
 
-    formated_data["data_pregao"] = formated_data["data_pregao"].map(lambda a: doc["data_pregao"],)
-    formated_data["data_liquidacao"] = formated_data["data_liquidacao"].map(lambda a: doc["data_liquidacao"])
-    formated_data["nome_ativo"] = transactions["titulo"]
-    formated_data["ticker"] = formated_data["nome_ativo"]
-    for ativo in formated_data["nome_ativo"].unique():
-        formated_data["ticker"] = formated_data["ticker"].replace(ativo, get_stock_data(ativo, "symbol"))
+    if doc["corr"] == "xp":
 
-    formated_data["quantidade"] = transactions["quantidade"]
-    formated_data["un"] = transactions["preco"]
-    formated_data["total"] = transactions["total"]
-    formated_data["despesas_operacionais"] = formated_data["quantidade"] * doc["per_quote_tax"]
-    formated_data["preco_medio"] = (formated_data["total"] + formated_data["despesas_operacionais"])/transactions["quantidade"]
-    formated_data["tipo_de_operacao"] = transactions["obs"].map(lambda a: "DAY_TRADE" if "D" in a else "SWING_TRADE")
+        formated_data["data_pregao"] = formated_data["data_pregao"].map(lambda a: doc["data_pregao"],)
+        formated_data["data_liquidacao"] = formated_data["data_liquidacao"].map(lambda a: doc["data_liquidacao"])
+        formated_data["nome_ativo"] = transactions["titulo"]
+        formated_data["ticker"] = formated_data["nome_ativo"]
+        for ativo in formated_data["nome_ativo"].unique():
+            formated_data["ticker"] = formated_data["ticker"].replace(ativo, get_stock_data(ativo, "symbol"))
 
-    mask_day_trade = formated_data["tipo_de_operacao"] == "DAY_TRADE"
-    mask_swing_trade = ~mask_day_trade
+        formated_data["quantidade"] = transactions["quantidade"]
+        formated_data["un"] = transactions["preco"]
+        formated_data["total"] = transactions["total"]
+        formated_data["despesas_operacionais"] = formated_data["quantidade"] * doc["per_quote_tax"]
+        formated_data["preco_medio"] = (formated_data["total"] + formated_data["despesas_operacionais"])/transactions["quantidade"]
+        formated_data["tipo_de_operacao"] = transactions["obs"].map(lambda a: "DAY_TRADE" if "D" in a else "SWING_TRADE")
 
-    formated_data["irrf_swing_trade"] = np.where(mask_swing_trade, 0, np.where(
-        mask_swing_trade & ((formated_data["total"] - formated_data["despesas_operacionais"]) * 0.0005 >= 1),
-        round((formated_data["total"] - formated_data["despesas_operacionais"]) * 0.0005, 2), 0))
-    formated_data["irrf_day_trade"] = np.where(mask_day_trade, np.where(
-        (formated_data["total"] - formated_data["despesas_operacionais"]) * 0.01 >= 1,
-        round((formated_data["total"] - formated_data["despesas_operacionais"]) * 0.01, 2), 0), 0)
+        mask_day_trade = formated_data["tipo_de_operacao"] == "DAY_TRADE"
+        mask_swing_trade = ~mask_day_trade
 
-    del mask_swing_trade
-    del mask_day_trade
+        formated_data["irrf_swing_trade"] = np.where(mask_swing_trade, 0, np.where(
+            mask_swing_trade & ((formated_data["total"] - formated_data["despesas_operacionais"]) * 0.0005 >= 1),
+            round((formated_data["total"] - formated_data["despesas_operacionais"]) * 0.0005, 2), 0))
+        formated_data["irrf_day_trade"] = np.where(mask_day_trade, np.where(
+            (formated_data["total"] - formated_data["despesas_operacionais"]) * 0.01 >= 1,
+            round((formated_data["total"] - formated_data["despesas_operacionais"]) * 0.01, 2), 0), 0)
 
-    formated_data.round(2)
-    formated_data["mercado"] = transactions["tipo_mercado"]
-    formated_data["nome_corretora"] = formated_data["nome_corretora"].map(lambda a: doc["nome_corretora"])
-    formated_data["cnpj_corretora"] = formated_data["cnpj_corretora"].map(lambda a: doc["cnpj_corretora"])
-    formated_data["cv"] = transactions["cv"]
-    del transactions
-    del ativo
-    formated_data["nome_empresa"] = formated_data["nome_ativo"].map(lambda a: get_stock_data(a, "shortname"))
+        del mask_swing_trade
+        del mask_day_trade
 
-    formated_data["cnpj_empresa"] = formated_data["nome_empresa"].map(lambda a: read_db(a, "cnpj"))
+        formated_data.round(2)
+        formated_data["mercado"] = transactions["tipo_mercado"]
+        formated_data["nome_corretora"] = formated_data["nome_corretora"].map(lambda a: doc["nome_corretora"])
+        formated_data["cnpj_corretora"] = formated_data["cnpj_corretora"].map(lambda a: doc["cnpj_corretora"])
+        formated_data["cv"] = transactions["cv"]
+        del transactions
+        del ativo
+        formated_data["nome_empresa"] = formated_data["nome_ativo"].map(lambda a: get_stock_data(a, "longname"))
 
-    return formated_data
+        formated_data["cnpj_empresa"] = formated_data["ticker"].map(lambda a: read_db(a))
+
+        return formated_data
+
+    if doc["corr"] == "avenue":
+        cr = CurrencyRates()
+        formated_data["data_pregao"] = transactions["data_pregao"]
+        formated_data["data_liquidacao"] = transactions["data_liquidacao"]
+        formated_data["ticker"] = transactions["ticker"]
+        formated_data["nome_ativo"] = transactions["nome"]
+        formated_data["quantidade"] = transactions["quantidade"]
+        formated_data["un"] = pd.to_numeric(transactions["un"]).round(3)
+        formated_data["un"] = formated_data["un"].map(lambda a: cr.convert("USD", "BRL", float(a))).replace(",", "\.", regex=True)
+        formated_data["total"] = transactions["total"]
+        formated_data["total"] = formated_data["total"].map(lambda a: cr.convert("USD", "BRL", float(a))).replace(",", "\.", regex=True)
+        transactions["despesas_operacionais"] = transactions["corretagem"] + transactions["taxa_transacao"] + transactions["taxa_outras"]
+
+        columns_to_convert = ["un", "total", "despesas_operacionais", "preco_medio"]
+
+        # Perform currency conversion first
+        formated_data[columns_to_convert] = cr.convert("USD", "BRL", transactions[columns_to_convert].astype(float))
+
+        # Convert to numeric, round, and convert to string
+        formated_data[columns_to_convert] = formated_data[columns_to_convert].round(3).astype(str)
+
+        # Remove periods and commas
+        formated_data[columns_to_convert] = formated_data[columns_to_convert].replace(["\.", ","], "", regex=True)
+
+        # Convert back to float and divide by 100
+        formated_data[columns_to_convert] = formated_data[columns_to_convert].astype(float) / 100
+
+
+        formated_data = formated_data.assign(
+            irrf_day_trade=0,
+            irrf_swing_trade=0,
+            tipo_de_operacao="-",
+            mercado="-",
+            nome_corretora="AVENUE SECURITIES LLC",
+            cnpj_corretora="61.384.004/0001-05",
+            cnpj_empresa="-"
+        )
+
+        formated_data["cv"] = transactions["cv"]
+        formated_data["nome_empresa"] = transactions["ticker"].map(lambda a: get_stock_data(a, "longname"))
+
+        return formated_data
+
 
 
 def main():
 
     with ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
         documents = list(executor.map(render_pdf, glob.glob("runtime/in/*.pdf")))
+
+
+    for i, d in enumerate(documents):
+        if d == None:
+            del documents[i]
 
     with ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
         parsed_documents = list(executor.map(parse_data, documents))
@@ -266,5 +488,6 @@ def main():
 
 
 if __name__ == "__main__":
+    ocr_check()
     main()
     print(time.time() - start)
